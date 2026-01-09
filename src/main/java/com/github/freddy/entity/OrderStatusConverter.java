1baba303c57b2c6986854bf8fb5d5f45351a7c17
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 09:51:56 +0100

    feat: classe OrderStatusConverter add

--- src/main/java/com/github/freddy/entity/OrderStatusConverter.java
+++ src/main/java/com/github/freddy/entity/OrderStatusConverter.java
@@ -1,4 +1,25 @@
 package com.github.freddy.entity;
 
-public class OrderStatusConverter {
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true) // aplica automaticamente a todas as entidades com OrderStatus
+public class OrderStatusConverter implements AttributeConverter<OrderStatus, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(OrderStatus status) {
+        if (status == null) {
+            return null;
+        }
+        return status.getCode();
+    }
+
+    @Override
+    public OrderStatus convertToEntityAttribute(Integer code) {
+        if (code == null) {
+            return null;
+        }
+        return OrderStatus.fromCode(code); // método do enum
+    }
 }
+

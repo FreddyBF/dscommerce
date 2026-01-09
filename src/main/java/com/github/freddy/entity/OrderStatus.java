54cb75c106284dad54bf1e2292af5bec947db010
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 06:45:46 +0100

    feat: entity OrderStatus add

--- src/main/java/com/github/freddy/entity/OrderStatus.java
+++ src/main/java/com/github/freddy/entity/OrderStatus.java
@@ -1,4 +1,10 @@
 package com.github.freddy.entity;
 
 public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID, // Pagamento aprovado
+    SHIPPED, //Pedido enviado para entrega
+    DELIVERED, // Pedido entregue ao cliente
+    CANCELED; //Pedido cancelado (antes da entrega)
 }
+

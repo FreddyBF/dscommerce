a2988bff3d9977d073fe1b883b6d6a6e4437f024
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 06:45:21 +0100

    feat: entity Order add

---
+++ src/main/java/com/github/freddy/entity/Order.java
@@ -0,0 +1,40 @@
+package com.github.freddy.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ORDERS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order { //Pedido do cliente
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant creationDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @PrePersist
+    public void prePersist() {
+        /*
+        Marca um método que será chamado automaticamente antes
+        de salvar uma entidade no banco pela primeira vez (INSERT)
+        * */
+        creationDate = Instant.now();
+    }
+
+
+}

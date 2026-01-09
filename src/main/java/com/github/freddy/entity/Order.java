5db36cd23b5cea983d698b52bb3780a0ead4cd6f
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 12:34:21 +0100

    feat: mapping the order with the orderItem entity

--- src/main/java/com/github/freddy/entity/Order.java
+++ src/main/java/com/github/freddy/entity/Order.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -30,6 +32,10 @@ public class Order { //Pedido do cliente
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+
+    @OneToMany(mappedBy = "id.order", cascade = CascadeType.ALL)
+    private Set<OrderItem> items = new HashSet<>();
 
     @PrePersist
     public void prePersist() {

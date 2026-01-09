67343e8d175aed6cbfbbb359d7b2db8dd432d90e
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 09:52:44 +0100

    feat: entity Payment add

--- src/main/java/com/github/freddy/entity/Payment.java
+++ src/main/java/com/github/freddy/entity/Payment.java
@@ -1,4 +1,28 @@
 package com.github.freddy.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PAYMENTS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Payment {
+    private UUID id;
+    private Instant paymentDate;
+
+    @PrePersist
+    public void prePersist() {
+        paymentDate = Instant.now();
+    }
 }

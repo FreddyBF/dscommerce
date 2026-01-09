4b34844fed78433a40addffc2c5f17c93fdf608a
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 06:22:36 +0100

    feat: entity product add

---
+++ src/main/java/com/github/freddy/entity/Product.java
@@ -0,0 +1,31 @@
+package com.github.freddy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String name;
+    @Column(columnDefinition = "TEXT")
+
+    private String description;
+
+    private BigDecimal price;
+    
+    private String imgUrl;
+}

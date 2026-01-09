d212fe956ffda59a3b2c35bc6faee47a75760ddf
Alfredo <baptistaalfredo81@gmail.com>
2026-01-09 09:50:03 +0100

    feat: entity OrderItemPK add

--- src/main/java/com/github/freddy/entity/OrderItemPK.java
+++ src/main/java/com/github/freddy/entity/OrderItemPK.java
@@ -1,4 +1,28 @@
 package com.github.freddy.entity;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class OrderItemPK {
+    /*
+    Como o OrderItem não tem um ID simples (como 1, 2, 3), mas sim uma Chave Composta (ID do Pedido + ID do Produto),
+    precisamos de uma classe auxiliar para a chave.A. A Chave Composta (PK)
+    * */
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }

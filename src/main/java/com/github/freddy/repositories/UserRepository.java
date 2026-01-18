1058f95086ea846bdf6dd570392d53158c11ecae
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:21:32 +0100

    feat: add method to verify if email already exists

--- src/main/java/com/github/freddy/repositories/UserRepository.java
+++ src/main/java/com/github/freddy/repositories/UserRepository.java
@@ -2,8 +2,16 @@ package com.github.freddy.repositories;
 
 import com.github.freddy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
+    Optional<UserDetails> findByEmail(@Param("email") String email);
+
+    Boolean existsByEmail(String email);
 }

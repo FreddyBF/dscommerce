97901ee901848abcba21fbb6a862d326de158581
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:20:16 +0100

    feat: add custom exception for denied

--- src/main/java/com/github/freddy/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/github/freddy/security/CustomAccessDeniedHandler.java
@@ -1,4 +1,40 @@
 package com.github.freddy.security;
 
-public class CustomAccessDeniedHandler {
+import com.github.freddy.dtos.errors.StandardError;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Instant;
+
+
+@Component
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, java.io.IOException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        StandardError err = new StandardError(
+                Instant.now(),
+                HttpServletResponse.SC_FORBIDDEN,
+                "Você não tem permissão para acessar este recurso.",
+                request.getRequestURI()
+        );
+
+        response.getWriter().write(objectMapper.writeValueAsString(err));
+    }
 }

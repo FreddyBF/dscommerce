ccaf5fc63033e3fd75a3c282b0d57903d3a20e5b
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:25:10 +0100

    feat: add payment repository

--- src/main/java/com/github/freddy/repositories/PaymentRepository.java
+++ src/main/java/com/github/freddy/repositories/PaymentRepository.java
@@ -1,4 +1,9 @@
 package com.github.freddy.repositories;
 
-public interface PaymentRepository {
+import com.github.freddy.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }

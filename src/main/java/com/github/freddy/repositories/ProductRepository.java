ced384f4d2a7c763a117da474a96698b296f9688
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:23:05 +0100

    feat: add method to findByName

--- src/main/java/com/github/freddy/repositories/ProductRepository.java
+++ src/main/java/com/github/freddy/repositories/ProductRepository.java
@@ -2,8 +2,14 @@ package com.github.freddy.repositories;
 
 import com.github.freddy.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :productName, '%'))")
+    List<Product> findByProductNameIgnoreCaseContaining(String productName);
 }

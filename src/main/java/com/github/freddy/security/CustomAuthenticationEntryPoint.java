5fa59a5217075172c1322f6719eefcbaafb9df89
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:19:48 +0100

    feat: add custom exception for entry point

--- src/main/java/com/github/freddy/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/github/freddy/security/CustomAuthenticationEntryPoint.java
@@ -1,4 +1,39 @@
 package com.github.freddy.security;
 
-public class CustomAuthenticationEntryPoint {
+import com.github.freddy.dtos.errors.StandardError;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper; // Jackson para converter Objeto em JSON
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, java.io.IOException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        // Usando o seu DTO StandardError para manter o padrão
+        StandardError err = new StandardError(
+                Instant.now(),
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Credenciais inválidas ou token ausente.",
+                request.getRequestURI()
+        );
+
+        // O writeValue escreve o JSON direto no corpo da resposta
+        response.getWriter().write(objectMapper.writeValueAsString(err));
+    }
 }

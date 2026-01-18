211f875d44b0e6807c5fd5ba6e84f68c6ff9086c
Alfredo <baptistaalfredo81@gmail.com>
2026-01-18 19:19:16 +0100

    feat: add custom implementation of userServiceDetails

--- src/main/java/com/github/freddy/security/CustomUserDetails.java
+++ src/main/java/com/github/freddy/security/CustomUserDetails.java
@@ -1,4 +1,27 @@
 package com.github.freddy.security;
 
-public class CustomUserDetails {
+import com.github.freddy.exceptions.ResourceNotFoundException;
+import com.github.freddy.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetails(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
